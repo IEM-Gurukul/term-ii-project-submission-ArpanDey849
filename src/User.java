4d6004e722a0d82bc8521be3b9cde167407893af
ArpanDey <deyarpan430@gmail.com>
2026-03-24 19:30:22 +0530

    Add files via upload

---
+++ src/User.java
@@ -0,0 +1,23 @@
+package user;
+
+public abstract class User {
+    protected String userId;
+    protected String name;
+    protected String email;
+    
+    public User(String userId, String name, String email) {
+        this.userId = userId;
+        this.name = name;
+        this.email = email;
+    }
+    
+    public abstract String getUserRole();
+    
+    public String getUserId() {
+        return userId;
+    }
+    
+    public String getName() {
+        return name;
+    }
+}
